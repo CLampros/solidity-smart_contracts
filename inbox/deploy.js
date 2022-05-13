8dc7f4b9669060048ac4cf025e2ae618e3f8d946
Lampros Chrysovergis <lampros.chrysovergis@kyndryl.com>
2022-05-13 19:32:06 +0200

    continue...

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,8 +1,19 @@
-const path = require('path');
-const fs = require('fs');
-const solc = require('solc');
+// const path = require('path');
+// const fs = require('fs');
+// const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+// const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+// const source = fs.readFileSync(inboxPath, 'utf8');
 
-modules.exports = solc.compile(source, 1).contracts[':Inbox'];
+// modules.exports = solc.compile(source, 1).contracts[':Inbox'];
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'prepare bullet voice abandon shoe wild cabbage float tired snow side laptop',
+    'https://rinkeby.infura.io/v3/71574909fcfa4817a86fa97308b3ddb9'
+);
+
+const web3 = new Web3(provider);
